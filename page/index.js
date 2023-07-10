b11f9c76683c052a1cf69f14810ecd70f44bf118
KimEunA97 <kimeuna9712@gmail.com>
2023-07-10 16:12:02 +0900

    5초마다 별 찍기

--- page/index.js
+++ page/index.js
@@ -2,25 +2,30 @@ const root = document.getElementById('root')
 
 // const star = document.getElementsByClassName('star')
 
-function getRandomPosition(element) {
+function timeSetStars() {
 
-  let diffX = document.body.offsetWidth - element.clientWidth;
-  let diffY = document.body.offsetHeight - element.clientHeight
-  var maxX = document.body.offsetWidth;
-  var maxY = document.body.offsetHeight;
-  var randomX = Math.floor(Math.random() * maxX);
-  var randomY = Math.floor(Math.random() * maxY);
-  return [randomX, randomY];
-}
+  function getRandomPosition() {
+    const maxX = document.body.offsetWidth;
+    const maxY = document.body.offsetHeight;
+    let randomX = Math.floor(Math.random() * maxX);
+    let randomY = Math.floor(Math.random() * maxY);
+    return [randomX, randomY];
+  }
 
-function setRandomPosition(element) {
-  var position = getRandomPosition(element);
-  console.log(position);
-  element.style.left = position[0] + 'px';
-  element.style.top = position[1] + 'px';
-}
+  function setRandomPosition(element) {
+    var position = getRandomPosition(element);
+    console.log(position);
+    element.style.left = position[0] + 'px';
+    element.style.top = position[1] + 'px';
+  }
 
-const elements = document.getElementsByClassName('star');
-for (let i = 0; i < elements.length; i++) {
-  setRandomPosition(elements[i]);
+  const elements = document.getElementsByClassName('star');
+  for (let i = 0; i < elements.length; i++) {
+    setRandomPosition(elements[i]);
+  }
 }
+setInterval(() => {
+  timeSetStars();
+
+}, 5000);
+timeSetStars();
