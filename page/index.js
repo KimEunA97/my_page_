2f9621e0bf9b5d740724054295b0e0e0f2be6e98
KimEunA97 <kimeuna9712@gmail.com>
2023-07-10 15:50:22 +0900

    랜덤 위치로 별 찍히기는 함.

--- page/index.js
+++ page/index.js
@@ -2,21 +2,22 @@ const root = document.getElementById('root')
 
 // const star = document.getElementsByClassName('star')
 
-
-
 function getRandomPosition(element) {
-  const x = document.body.offsetWidth - element.clientWidth;
-  const y = document.body.offsetHeight - element.clientHeight;
-  let randomX = Math.floor(Math.random() * x);
-  let randomY = Math.floor(Math.random() * y);
+
+  let diffX = document.body.offsetWidth - element.clientWidth;
+  let diffY = document.body.offsetHeight - element.clientHeight
+  var maxX = document.body.offsetWidth;
+  var maxY = document.body.offsetHeight;
+  var randomX = Math.floor(Math.random() * maxX);
+  var randomY = Math.floor(Math.random() * maxY);
   return [randomX, randomY];
 }
 
 function setRandomPosition(element) {
-  const position = getRandomPosition(element);
-  console.dir(position)
-  element.style.top = position[0] + 'px';
-  element.style.left = position[1] + 'px';
+  var position = getRandomPosition(element);
+  console.log(position);
+  element.style.left = position[0] + 'px';
+  element.style.top = position[1] + 'px';
 }
 
 const elements = document.getElementsByClassName('star');
