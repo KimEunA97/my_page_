380175fd3cbe4aa34751c39ace14f8afb81bf319
KimEunA97 <kimeuna9712@gmail.com>
2023-08-09 03:57:41 +0900

    엔트리파일 수정 시도중

--- dist/main.js
+++ dist/main.js
@@ -7,8 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./page/index.html":
+/*!*************************!*\
+  !*** ./page/index.html ***!
+  \*************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (1:0)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> <!DOCTYPE html>\\n| <html>\\n| \");\n\n//# sourceURL=webpack:///./page/index.html?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./page/index.html"]();
 /******/ 	
 /******/ })()
 ;
